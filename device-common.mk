LineageOS/android_device_google_muskie

walleye: Set chre_daemon_dsp_library
Change-Id: If0a2be3eee2f55fcbb48ca9118f4f14f68ac1c32

device-common.mk
@@ -110,6 +110,9 @@ PRODUCT_PACKAGES += \
     libhidlbase-v32.vendor \
     libutils-v33
 
+# CHRE
+$(call soong_config_set,chre,chre_daemon_dsp_library,//vendor/google/taimen:libsdsprpc)
+
 # DRM
 PRODUCT_PACKAGES += \
     libcrypto_shim.vendor

LineageOS/android_device_google_walleye

device-common.mk
@@ -110,6 +110,9 @@ PRODUCT_PACKAGES += \
     libhidlbase-v32.vendor \
     libutils-v33
 
+# CHRE
+$(call soong_config_set,chre,chre_daemon_dsp_library,//vendor/google/walleye:libsdsprpc)
+
 # DRM
 PRODUCT_PACKAGES += \
     libcrypto_shim.vendor