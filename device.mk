eac56f7159b2b7386026b73af3f8f76721f74e19
Nolen Johnson <johnsonnolen@gmail.com>
2025-04-02 01:28:40 -0400

    walleye: Remove dumb overlay inheritance warning
    Change-Id: I49bfd202d48e4d9ea76c223c3d5f337d2244a054

--- device.mk
+++ device.mk
@@ -23,12 +23,6 @@ PRODUCT_HARDWARE := walleye
 # To build walleye specific modules e.g. librecovery_ui_walleye.
 PRODUCT_SOONG_NAMESPACES += $(LOCAL_PATH)
 
-# DEVICE_PACKAGE_OVERLAYS for the device should be before
-# including common overlays since the one listed first
-# takes precedence.
-ifdef DEVICE_PACKAGE_OVERLAYS
-$(warning Overlays defined in '$(DEVICE_PACKAGE_OVERLAYS)' will override '$(PRODUCT_HARDWARE)' overlays)
-endif
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
 # Audio
